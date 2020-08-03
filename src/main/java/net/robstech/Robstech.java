dbf1cac338938ec8fa6ecd7d789b97c90139cd7a
r0b1n <31573718+r0binius@users.noreply.github.com>
2020-08-03 15:16:44 +0200

    Tin Implementation

--- src/main/java/net/robstech/Robstech.java
+++ src/main/java/net/robstech/Robstech.java
@@ -1,18 +1,25 @@
 package net.robstech;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class Robstech implements ModInitializer {
-    
-    public static final Item TIN_ORE = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+
+    public static final Block TIN_ORE = new Block(FabricBlockSettings.of(Material.METAL).hardness(4.0f));
+    public static final Item TIN_INGOT = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
     
     @Override
     public void onInitialize() {
-        Registry.register(Registry.ITEM, new Identifier("robstech", "tin_ore"), TIN_ORE);
+        Registry.register(Registry.BLOCK, new Identifier("robstech", "tin_ore"), TIN_ORE);
+        Registry.register(Registry.ITEM, new Identifier("robstech", "tin_ore"), new BlockItem(TIN_ORE, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS)));
+        Registry.register(Registry.ITEM, new Identifier("robstech", "tin_ingot"), TIN_INGOT);
     }
 
 }
